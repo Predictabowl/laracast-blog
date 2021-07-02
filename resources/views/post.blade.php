3a622093e5e4475c9f1765de3789edbe3e45b500
Piero <piero.aldinucci@gmail.com>
2021-07-02 11:50:25 +0200

    End of Lesson 14

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,18 @@
 <!doctype html>
 
-<html>
-    <head>
-        <title>Post</title>
-        <link rel="stylesheet" href="/css/app.css"/>
-    </head>
-    <body>
-        <article>
-            <h1><?= $post->getTitle(); ?></h1>
-            <div>
-                <?= $post->getBody() ?>
-            </div>
-        </article>
-        <a href="/">Go Back</a>
-    </body>
-</html>
+
+<head>
+    <title>Post</title>
+    <link rel="stylesheet" href="/css/app.css"/>
+</head>
+<body>
+    <article>
+        <h1>{{ $post->getTitle(); }}</h1>
+        <div>
+            {!! $post->getBody() !!}
+
+        </div>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,19 +6,27 @@
         <link rel="stylesheet" href="/css/app.css"/>
     <body>
 
-        <?php foreach ($posts as $post): ?>
-            <article>
+        
+        @foreach ($posts as $post)
+        <article class="{{$loop->even ? 'foobar' : ''}}">
+                <!--@dd($loop)-->
                 <h1>
-                    <a href="/post/<?= $post -> getSlug(); ?>">
-                        <?= $post->getTitle(); ?>
+                    <a href="/post/{{ $post -> getSlug(); }}">
+                        {{ $post -> getTitle() }}
                     </a>
                 </h1>
 
                 <div>
-                    <?= $post->getExcerpt(); ?>
+                    {{ $post->getExcerpt(); }}
+                    <br>
+                    @if($loop->even)
+                        This is even
+                    @else
+                        This is odd
+                    @endif
                 </div>
             </article>
-        <?php endforeach; ?>
+        @endforeach;
 
     </body>
 </html>
