c36cd58faeb080b7554d9fb59beb8526e0feb5f3
Piero <piero.aldinucci@gmail.com>
2021-07-04 18:51:51 +0200

    End of Lesson 32

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,21 +12,23 @@ class Post extends Model
     protected $fillable = ["title","excerpt","body","slug","category_id","user_id"];
 //    protected $guarded = ["id"];
 //    protected $guarded = []; // This means that no field is guarded, so every field can be mass filled
-    
-     /* The following is for eager loading, 
-        if it's set like that it will ALWAYS eager load.
-        If you only need eager load in specific situation is better to use
-        'load' and 'with' when doing queries.
-        None the less it can be disabled using 'without(["author","category"])' in the query.
-        The better solution is to create a repository for working with entities.
-     */
+
+    /* The following is for eager loading,
+       if it's set like that it will ALWAYS eager load.
+       If you only need eager load in specific situation is better to use
+       'load' and 'with' when doing queries.
+       None the less it can be disabled using 'without(["author","category"])' in the query.
+       The better solution is to create a repository for working with entities.
+    */
 //    protected $with = ["author","category"];
 
-    public function category() {
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author(){
+    public function author()
+    {
         return $this->belongsTo(User::class, "user_id");
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
 <x-layout>
 
-@include("_post-header")
+    @include("_post-header")
 
-<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card/>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts yet.</p>
+        @endif
 
-
-    <div class="lg:grid lg:grid-cols-2">
-        <x-post-card/>
-        <x-post-card/>
-    </div>
-
-    <div class="lg:grid lg:grid-cols-3">
-        <x-post-card/>
-        <x-post-card/>
-        <x-post-card/>
-
-    </div>
 </main>
 
 {{--         @foreach ($posts as $post)
