33a1a5f2d19bf3e27ba93437cae0d388c2ce8d12
Piero <piero.aldinucci@gmail.com>
2021-07-04 12:49:19 +0200

    End of Lesson 31

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,25 @@
 <x-layout>
-    
-        @foreach ($posts as $post)
+
+@include("_post-header")
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card/>
+
+
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card/>
+        <x-post-card/>
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card/>
+        <x-post-card/>
+        <x-post-card/>
+
+    </div>
+</main>
+
+{{--         @foreach ($posts as $post)
         <article class="{{$loop->even ? 'foobar' : ''}}">
                 <h1>
                     <a href="/post/{{ $post -> slug }}">
@@ -23,6 +42,6 @@
                     @endif
                 </div>
             </article>
-        @endforeach
+            @endforeach --}}
 
-</x-layout>
+        </x-layout>
