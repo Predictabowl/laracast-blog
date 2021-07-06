3181da4b8457a78b7405d91fb19451c9f0eb5f1e
Piero <piero.aldinucci@gmail.com>
2021-07-06 19:06:35 +0200

    End of lesson 53

---
+++ database/migrations/2021_07_06_183610_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text("body");
+            $table->timestamps();
+            $table->foreignId("post_id")->constrained()->cascadeOnDelete(); // it's the same as the 2 rows below
+            $table->foreignId("user_id")->constrained()->cascadeOnDelete(); // it's the same as the 2 rows below
+
+            /*$table->unsignedBigInteger("post_id"); //must be the same type as the Post id
+            $table->foreign("post_id")->references("id")->on("posts")->cascadeOnDelete();*/
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
