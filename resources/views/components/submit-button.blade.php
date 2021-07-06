0d33ba5f2feed42c9ff29aea08e665a0c90d75c9
Piero <piero.aldinucci@gmail.com>
2021-07-06 23:13:58 +0200

    End lesson 57

---
+++ resources/views/components/submit-button.blade.php
@@ -0,0 +1,4 @@
+<button type="submit"
+    class="bg-blue-500 text-white font-semibold text-sm py-2 px-10 uppercase rounded-2xl hover:bg-blue-600">
+    {{ $slot }}
+</button>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -57,31 +57,7 @@
 
             
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                        <x-panel>
-                            <form method="POST" action="/post/{{$post->slug}}/comments">
-                                @csrf
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" width="40" height="40" class="rounded-full">
-                                    <h2 class="ml-3">Hai qualcosa da dire?</h2>
-                                </header>
-                                <div class="mt-4">
-                                    <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Scrivi un commento."></textarea>
-                                </div>
-                                <div class="flex justify-end mt-4 pt-4 border-t border-gray-200">
-                                    <button type="submit"
-                                        class="bg-blue-500 text-white font-semibold text-sm py-2 px-10 uppercase rounded-2xl hover:bg-blue-600">
-                                        Posta
-                                    </button>
-                                </div>
-                            </form>
-                        </x-panel>
-                    @else
-                        <p class="font-semibold">
-                            <a href="/register" class="hover:underline text-blue-500">Register</a> or
-                            <a href="/login" class="hover:underline text-blue-500">Log in</a> to leave a comment.
-                        </p>
-                    @endauth
+                    @include("posts._add-comment-form")
                     @foreach($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
                     @endforeach
