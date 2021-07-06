bd25998d013a6df0899b73f6d49be4cce8a9bfe3
Piero <piero.aldinucci@gmail.com>
2021-07-06 15:12:56 +0200

    End of lesson 50

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,12 @@ use Illuminate\Support\Facades\Route;
 
   Route::post("register",[RegisterController::class,"store"])->middleware("guest");
 
+  Route::get("login",[SessionController::class,"create"])->middleware("guest");
+
+  Route::post("login",[SessionController::class,"store"])->middleware("guest");  
+
   Route::post("logout",[SessionController::class,"destroy"])->middleware("auth");
+
 
 /*  Route::get(
       "categories/{category:slug}",
